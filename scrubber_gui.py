9caa6a43f4c6b62987828ee647c11abddc94f665
Mickey Muzi Li <mickeylee524@gmail.com>
2016-01-22 09:47:05 -0800

    code cleaned up

--- scrubber_gui.py
+++ scrubber_gui.py
@@ -1,6 +1,6 @@
 from tkinter import *
-from tkinter import ttk
-from tkinter.filedialog import askopenfilename
+# from tkinter import ttk
+# from tkinter.filedialog import askopenfilename
 from tkinter.filedialog import askdirectory
 import scrubber
 
